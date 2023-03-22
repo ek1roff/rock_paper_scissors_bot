b62543ae9d1c1228b08a0ca6e1a3f5ed845dc22c
Kirill <kerlykiv@gmail.com>
2023-03-22 15:05:09 +0700

    + set_menu

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from keyboards.set_menu import set_main_menu
 
 logger = logging.getLogger(__name__)
 
@@ -26,6 +27,7 @@ async def main():
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+    await set_main_menu(bot)
 
 
 if __name__ == '__main__':
